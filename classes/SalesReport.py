2e20bac428db693fe7b018b16ba18bb7ccbc7939
KrakeKrake <42541628+KrakeKrake@users.noreply.github.com>
2025-11-05 16:54:02 +1100

    Created automatic terminal print formatting

--- classes/SalesReport.py
+++ classes/SalesReport.py
@@ -9,17 +9,93 @@ class SalesReport:
     def __init__(self):
         self.db = DBManager()
 
-    def print_as_table(self, data, columns):
+    def print_as_table(self, title, keys, data, size = 80):
         """
         Data is the data
         Columns should be an int list, listing the indexes the columns should appear in
         """
+        # List out the nice colours
+        # Reset
+        r = "\033[0m"
+        # Dark black Grey
+        dg = "\033[48;2;25;25;25m"
+        # Light black Grey
+        lg = "\033[48;2;40;40;40m"
+        # Dark white-grey...
+        dw = "\033[48;2;60;60;60m"
+        # Light white-grey
+        lw = "\033[48;2;100;100;100m"
+        # Green
+        gr = "\033[38;2;144;238;144m"
+        # Blue
+        bl = "\033[38;2;173;216;230m"
+        # Yellow
+        yl = "\033[38;2;255;255;179m"
+        # Pink
+        pk = "\033[38;2;255;182;193m"
+        # Purple
+        pr = "\033[38;2;216;191;216m"
+        # Orange (or is a keyword)
+        ora = "\033[38;2;255;204;153m"
+        # Cyan
+        cy = "\033[38;2;180;255;255m"
+        # So I can refereance them in colour numbers
+        # I don't think we will ever have more than 7 cols
+        colour_list = [gr, bl, yl, pk, pr, ora, cy]
+
         col_count = len(data[0])
 
-        pass
+        # Line template:
+        # print(f"{dw}{f"- {title} -":^80}{r}")
+        # Print the title in the light grey, dark white?
+        print(f"{lw}{f"- {title} -":#^{size}}{r}")
+        # Print out the key, do some fun calculations to have them evenly spread
+        num_keys = len(keys)
+        key_string = f"{dw}-----"
+        key_space = ((size - 20) // num_keys) - 1
+        key_spacing = (size - 20) // key_space
+        spare_space = size - ((key_space * num_keys) + (key_spacing * (num_keys - 1)) + 10 + (num_keys * 2))
+        print(key_space, key_spacing, spare_space)
+        for i, key in enumerate(keys):
+            key_string += f"{colour_list[i % len(colour_list)]} {key:^{key_space}} {r}"
+            if i != num_keys - 1:
+                key_string += f"{dw}{'-' * key_spacing}"
+        key_string += f"{dw}-{"-" * spare_space}----{r}"
+        print(f"{key_string:<{size}}{r}")
+
+        # Now for the actual data
+        for row_i, row in enumerate(data):
+
+            # Make the background colour to alternate
+            if row_i % 2 == 0:
+                bg = dg
+            else:
+                bg = lg
+
+            row_string = f"{bg}-----"
+            for col, item in enumerate(row):
+
+                if "id" in keys[col].lower():
+                    item = "#" + str(item)
+                elif "price" in keys[col].lower():
+                    item = f"${item:.2f}"
+                elif "cost" in keys[col].lower():
+                    item = f"${item:.2f}"
+                elif "revenue" in keys[col].lower():
+                    item = f"${item:.2f}"
+                row_string += f"{colour_list[col % len(colour_list)]} {item:<{key_space}} {r}"
+                if col != col_count - 1:
+                    row_string += f"{bg}{'-' * key_spacing}"
+            row_string += f"{bg}-{"-" * spare_space}----{r}"
+            print(row_string)
+
+
 
     # Count of all items sold ever
     def item_statistics(self):
+        """
+        :returns id, name, price, num sold, total price:
+        """
         # Want to look in orders to get all the items
         # WIth all those orders, look at the items
 
@@ -36,6 +112,10 @@ class SalesReport:
 
     # Return the stats for one item specifically.
     def stats_by_items(self, item):
+        """
+        :param item:
+        :returns order_id, date, item_id, name, price quantity:
+        """
         item_stat = self.db.execute("""
             SELECT orders.order_id, orders.date, items.id, items.name, items.price, stock.quantity
             FROM orders
@@ -48,6 +128,9 @@ class SalesReport:
 
     # Show all the items sold *sorted by* date
     def items_by_date(self):
+        """
+        :returns id, date, name, sum:
+        """
         item_stat = self.db.execute("""
             SELECT items.id, date(orders.date) AS date, items.name, SUM(stock.quantity)
             FROM orders
@@ -64,9 +147,10 @@ class SalesReport:
     def items_on_date(self, date = "2025-10-30"):
         """
         The date needs to be a perfect string as YYYY-MM-DD
+        :returns date, id, name, amount, made:
         """
         item_stat = self.db.execute("""
-            SELECT items.id, date(orders.date) AS date, items.name, SUM(stock.quantity)
+            SELECT items.id, items.name, SUM(stock.quantity), items.price * SUM(stock.quantity)
             FROM orders
             JOIN stock ON stock.order_id = orders.order_id
             JOIN items ON items.id = stock.id
@@ -78,23 +162,27 @@ class SalesReport:
 
 
     def orders(self):
+        """
+        :returns order_id, date, total:
+        """
         item_stat = self.db.execute("""
             SELECT stock.order_id, orders.date, orders.total
             FROM items 
             JOIN stock on items.id = stock.id
             JOIN orders on stock.order_id = orders.order_id
             WHERE orders.order_id != -1
+            GROUP BY orders.order_id
+            ORDER BY orders.order_id DESC 
         """).fetchall()
         return item_stat
 
-    def view_user_orders(userId: int):
-        db = DBManager()
-        user_orders = db.execute("""
-                                 SELECT *
-                                 FROM orders
-                                 WHERE user_id = ?
-                                 ORDER BY date DESC
-                                 """, (userId,)).fetchall()
+    def view_user_orders(self, user_id: int = 0):
+        user_orders = self.db.execute("""
+             SELECT *
+             FROM orders
+             WHERE user_id = ?
+             ORDER BY date DESC
+                                 """, (user_id,)).fetchall()
         r = "\033[0m"  # This is the colour key to reset the colour
         # Do some nice formating with every order, from earliest to latest
         for order in user_orders:
@@ -102,7 +190,7 @@ class SalesReport:
             # First, print the order info
             order_id, user_id, total, date, status = order
             print(f"{"\033[48;2;60;60;60m"}{f"Order #{order_id:} - Date: {date} - Status: {status}":<73}{r}")
-            items = db.execute("""
+            items = self.db.execute("""
                                SELECT items.name, items.price, stock.quantity
                                FROM stock
                                         JOIN items ON stock.id = items.id
@@ -134,4 +222,8 @@ class SalesReport:
 
 if __name__ == "__main__":
     report = SalesReport()
-    print(report.orders())
+    data = report.items_on_date()
+    report.print_as_table(f"Items sold on {"2025-10-30"}",
+                          ["Item ID", "Item Name", "Number sold", "Revenue"],
+                          data
+                          )
